c20e1bb0e64db8007ee82b73c1f19ace359fc858
SnorzMorz <68282318+SnorzMorz@users.noreply.github.com>
2021-04-21 18:21:11 +0100

    Added Files

---
+++ Ex1/Ex1/Date.cpp
@@ -0,0 +1,19 @@
+#include "..\..\..\..\Assignment 1 files\ex1\Date.h"
+
+class Date {
+	Date()
+	{
+		cout << "Date made!";
+
+	}
+	Date(unsigned int day, unsigned int month, unsigned int year)
+	{
+		d = day;
+
+	}
+	bool isLeapYear() {
+
+	}
+
+};
+
